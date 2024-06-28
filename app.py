1f6b4e82fb4b0aa7a394be3f23f4bf7bef2266bf
mell-isa <mellisaangelin@gmail.com>
2024-06-28 14:35:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -94,32 +94,33 @@ def eda_page():
     st.subheader('Exploring the transaction dataset')
 
     # Load the data
-    # data = pd.read_csv('onlinefrauddataset.csv')
+    dataset_url = "https://www.kaggle.com/datasets/rupakroy/online-payments-fraud-detection-dataset?select=PS_20174392719_1491204439457_log.csv"
+    data = pd.read_csv(dataset_url)
 
-    # st.write("### Dataset Overview")
-    # st.write(data.head())
+    st.write("### Dataset Overview")
+    st.write(data.head())
 
-    # st.write("### Basic Statistics")
-    # st.write(data.describe())
+    st.write("### Basic Statistics")
+    st.write(data.describe())
 
-    # st.write("### Distribution of Transaction Types")
-    # fig, ax = plt.subplots()
-    # sns.countplot(data['type'], ax=ax)
-    # st.pyplot(fig)
+    st.write("### Distribution of Transaction Types")
+    fig, ax = plt.subplots()
+    sns.countplot(data['type'], ax=ax)
+    st.pyplot(fig)
 
-    # non_numeric_columns = ['nameOrig', 'nameDest']
-    # data_numeric = data.drop(columns=non_numeric_columns)
-    # data_encoded = pd.get_dummies(data_numeric, columns=['type'])
+    non_numeric_columns = ['nameOrig', 'nameDest']
+    data_numeric = data.drop(columns=non_numeric_columns)
+    data_encoded = pd.get_dummies(data_numeric, columns=['type'])
 
-    # st.write("### Correlation Matrix")
-    # fig, ax = plt.subplots()
-    # sns.heatmap(data_encoded.corr(), annot=True, fmt=".2f", cmap="coolwarm", ax=ax)
-    # st.pyplot(fig)
+    st.write("### Correlation Matrix")
+    fig, ax = plt.subplots()
+    sns.heatmap(data_encoded.corr(), annot=True, fmt=".2f", cmap="coolwarm", ax=ax)
+    st.pyplot(fig)
 
-    # st.write("### Distribution of Transaction Amounts")
-    # fig, ax = plt.subplots()
-    # sns.histplot(data['amount'], bins=50, kde=True, ax=ax)
-    # st.pyplot(fig)
+    st.write("### Distribution of Transaction Amounts")
+    fig, ax = plt.subplots()
+    sns.histplot(data['amount'], bins=50, kde=True, ax=ax)
+    st.pyplot(fig)
 
 def about_page():
     st.title("About - Online Fraud Detection from kelompok 2")
